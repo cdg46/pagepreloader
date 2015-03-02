769b98a556be31f1629653d97832070b1a91d7dc
root <root@46-typo3-dev.ip-5-39-66.eu>
2015-03-02 15:26:49 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ajaxpagepreloader".
+ *
+ * Auto generated 02-03-2015 12:50
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Page Preloader',
+	'description' => 'Page Preloader. Ajax ou CSS.',
+	'category' => 'plugin',
+	'version' => '1.0.0',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => false,
+	'author' => 'Yann Bogdanovic',
+	'author_email' => 'support@cdgfpt46.fr',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.1.99-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
